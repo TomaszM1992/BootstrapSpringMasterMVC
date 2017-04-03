d5443b897c144adf83c325d4764f280f5f6da339
TomaszM1992 <tomek.m.osw@gmail.com>
2017-04-03 14:23:18 +0200

    Maven wrap

---
+++ src/main/java/BootstrapSpringMasterMVC/BootstrapSpringMasterMvcApplication.java
@@ -0,0 +1,12 @@
+package BootstrapSpringMasterMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapSpringMasterMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapSpringMasterMvcApplication.class, args);
+	}
+}
---
+++ src/test/java/BootstrapSpringMasterMVC/BootstrapSpringMasterMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package BootstrapSpringMasterMVC;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootstrapSpringMasterMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
